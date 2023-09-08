e4507b584e127b0a902463946c3d39ef1cc7ea35
Israel Ogboi <israel.imoleoluwa.kemi@gmail.com>
2023-09-08 19:19:38 +0100

    Add files via upload

---
+++ lamp.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo -e "\n\nUpdating Apt Packages and upgrading latest patches\n"
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+echo -e "\n\nInstalling Apache2 Web server\n"
+sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
+
+echo -e "\n\nAdding firewall rule to Apache\n"
+sudo ufw allow in "Apache"
+
+echo -e "\n\nInstalling PHP & Requirements\n"
+sudo apt-get install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php-pear php7.0-mcrypt php7.0-mysql -y
+
+echo -e "\n\nInstalling MySQL\n"
+sudo apt-get install mysql-server mysql-client -y
+
+echo -e "\n\nPermissions for /var/www\n"
+sudo chown -R www-data:www-data /var/www
+echo -e "\n\n Permissions have been set\n"
+
+echo -e "\n\nEnabling Modules\n"
+sudo a2enmod rewrite
+sudo phpenmod mcrypt
+
+sudo sed -i 's/DirectoryIndex index.html index.cgi index.pl index.xhtml index.htm/DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm/' /etc/apache2/mods-enabled/dir.conf
+
+echo -e "\n\nRestarting Apache\n"
+sudo service apache2 restart
+
+echo -e "\n\nLAMP Installation Completed"
+
+exit 0
+
+lamp_stack() {
+     cat << EOT >> $File
+    config.vm.provision "shell", inline: <<-SHELL
+
+EOT
+}
