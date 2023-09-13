39df44013e10dea410e4f3301a40cd430a6ec4d6
Israel Ogboi <israel.imoleoluwa.kemi@gmail.com>
2023-09-13 18:29:43 +0100

    Update msdeployment.sh

--- master_slave/msdeployment.sh
+++ master_slave/msdeployment.sh
@@ -65,11 +65,16 @@ cat << EOT >> $vFile
 		sudo systemctl start apache2
 
 		echo "Secure MySQL installation on VMs..."
-		sudo mysql_secure_installation
 		echo "Initializing MySQL with default user and password..."
-		sudo mysql -uroot -p$SQL_RPasswd -e \"CREATE USER '$SQL_dUser'@'localhost' IDENTIFIED BY '$SQL_dUserPasswd';\"
-		sudo mysql -uroot -p$SQL_RPasswd -e \"GRANT ALL PRIVILEGES ON . TO '$SQL_dUser'@'localhost' WITH GRANT OPTION;\""
-		sudo mysql -uroot -p$SQL_RPasswd -e \"FLUSH PRIVILEGES;\"
+		# Secure MySQL installation (set root password, remove anonymous users, etc.)
+		sudo mysql_secure_installation
+		# Create a new MySQL user
+		sudo mysql -uroot -p"$SQL_RPasswd" -e "CREATE USER '$SQL_dUser'@'localhost' IDENTIFIED BY '$SQL_dUserPasswd';"
+		# Grant all privileges to the new user
+		sudo mysql -uroot -p"$SQL_RPasswd" -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_dUser'@'localhost' WITH GRANT OPTION;"
+		# Flush privileges to apply the changes
+		sudo mysql -uroot -p"$SQL_RPasswd" -e "FLUSH PRIVILEGES;"
+
 
 		# Validate PHP functionality with Apache
 		echo "Generating PHP test file..."
@@ -109,4 +114,4 @@ sudo systemctl start apache2
 exit
 
 echo "Master and Slave VM deployed successfully :)"
-echo -e "$VM_Master: $IP_Master\n$VM_Slave: $IP_Slave"
+echo -e "$VM_Master: $IP_Master\n$VM_Slave:Â $IP_Slave"
